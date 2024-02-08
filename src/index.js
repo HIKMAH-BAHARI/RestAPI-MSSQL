d6c3f69188d9ecabfc957dbe1c3a27b4c0302d3a
Tri Yuli Yanto <141161254+maqlogaming57@users.noreply.github.com>
2024-02-08 19:17:54 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,14 +21,14 @@ const options = {
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
 <<<<<<< HEAD
-  origin: 'https://ereport-hikba.vercel.app', // Sesuaikan dengan asal aplikasi frontend Anda
+  origin: 'https://ereport-hikba.vercel.app', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 =======
   origin: 'http://localhost:3000',
   methods: 'GET,HEAD,POST',
 >>>>>>> f9eb113b0e325c2d03d84c61cc4e120fc5cbe58e
   credentials: true,
-  allowedHeaders: 'Content-Type,Authorization', // Tambahkan 'Authorization' ke daftar allowed headers
+  allowedHeaders: 'Content-Type,Authorization', 
 }));
 
 app.use((req, res, next) => {
