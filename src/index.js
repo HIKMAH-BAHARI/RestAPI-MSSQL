b8b139cc9a393219dfb0bc68b7e6cea5b2d883e0
maqlogaming57 <trijy7@gmail.com>
2024-12-04 07:27:38 +0100

    fix https

--- src/index.js
+++ src/index.js
@@ -24,19 +24,12 @@ const options = {
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: '*',
-  //origin: 'https://ereport-hikba.vercel.app',  
+  origin: 'https://ereport-hikba.vercel.app',  
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   allowedHeaders: 'Content-Type,Authorization', 
 }));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Credentials', true);  
-//   next();
-// });
 
 app.use(middlewareLogRequest);
 app.use(express.json());
