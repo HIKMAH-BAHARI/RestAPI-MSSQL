1aec13530f1954426d4f77d707b7dfb4898d02ec
maqlogaming57 <trijy7@gmail.com>
2024-12-24 07:11:28 +0100

    fix sertif

--- src/models/sertifs.js
+++ src/models/sertifs.js
@@ -4,81 +4,92 @@ const mssql = require('mssql');
 
 const searchSertifs = (body) => {
   const SQLQuery = `
-    SELECT TOP 3
-        TOFLMB.tgleff,
-        TOFLMB.nokontrak,
-        TOFLMB.acdrop,
-        TOFLMB.nama,
-        mCIF.alamat,
-        mCIF.kelurahan,
-        mCIF.kecamatan,
-        mCIF.kota,
-        mCIF.hp,
-        MCIFJOB.alamat AS alamatpt,
-        MCIFJOB.kota AS kotapt,
-        MCIFJOB.namapt,
-        TOFLMB.kdprd,
-        TOFLMB.kdaoh,
-        TOFLMB.frekmdl,
-        TOFLMB.mdlawal,
-        TOFLMB.mgnawal,
-        TOFRS.tagmgn,
-        TOFRS.tagmdl,
-        TOFRS.tagdnd,
-        TOFRS.tgltagih,
-        SUM(TOFRS.tagmgn + TOFRS.tagmdl + TOFRS.tagdnd) AS angsttl,
-        TOFRS.os AS sisa_angsuran,
-        TOFTABC.sahirrp,
-        TOFTABC.saldoblok,
-        TOFLMB.colbaru,
-        TOFLMB.kdcab
+    WITH RankedTOFRS AS (
+    SELECT
+        nokontrak,
+        tagmgn,
+        tagmdl,
+        tagdnd,
+        tgltagih,
+        os,
+        ROW_NUMBER() OVER (PARTITION BY nokontrak ORDER BY tgltagih ASC) AS row_num
     FROM
-        TOFLMB
-    LEFT JOIN
-        mCIF ON TOFLMB.nocif = mCIF.nocif
-    LEFT JOIN
-        TOFTABC ON TOFLMB.acdrop = TOFTABC.notab
-    LEFT JOIN
-        MCIFJOB ON mCIF.nocif = MCIFJOB.nocif
-    LEFT JOIN
-        TOFRS ON TOFLMB.nokontrak = TOFRS.nokontrak
+        TOFRS
     WHERE
-        (TOFLMB.nama LIKE '%' + @nama + '%' OR TOFLMB.nokontrak LIKE @nama)
-        AND TOFLMB.stsrec = 'A'
-        AND TOFRS.stsbyr = ''
-        AND TOFLMB.kdprd in (11, 12, 13, 16, 21, 22, 23, 25)
-    GROUP BY
-        TOFLMB.tgleff,
-        TOFLMB.nokontrak,
-        TOFLMB.acdrop,
-        TOFLMB.nama,
-        mCIF.alamat,
-        mCIF.kota,
-        mCIF.kelurahan,
-        mCIF.kecamatan,
-        mCIF.hp,
-        MCIFJOB.namapt,
-        MCIFJOB.alamat,
-        MCIFJOB.kota,
-        TOFLMB.kdprd,
-        TOFLMB.kdaoh,
-        TOFLMB.frekmdl,
-        TOFLMB.tglakad,
-        TOFLMB.mdlawal,
-        TOFLMB.mgnawal,
-        TOFLMB.angsmdl,
-        TOFLMB.angsmgn,
-        TOFTABC.sahirrp,
-        TOFTABC.saldoblok,
-        TOFLMB.colbaru,
-        TOFLMB.kdcab,
-        TOFRS.tagmgn,
-        TOFRS.tagmdl,
-        TOFRS.tagdnd,
-        TOFRS.tgltagih,
-        TOFRS.os
-    ORDER BY
-        TOFLMB.tglakad DESC;
+        stsbyr = ''
+)
+SELECT
+    TOFLMB.tgleff,
+    TOFLMB.nokontrak,
+    TOFLMB.acdrop,
+    TOFLMB.nama,
+    mCIF.alamat,
+    mCIF.kelurahan,
+    mCIF.kecamatan,
+    mCIF.kota,
+    mCIF.hp,
+    MCIFJOB.alamat AS alamatpt,
+    MCIFJOB.kota AS kotapt,
+    MCIFJOB.namapt,
+    TOFLMB.kdprd,
+    TOFLMB.kdaoh,
+    TOFLMB.frekmdl,
+    TOFLMB.mdlawal,
+    TOFLMB.mgnawal,
+    R.tagmgn,
+    R.tagmdl,
+    R.tagdnd,
+    R.tgltagih,
+    R.os AS sisa_angsuran,
+    SUM(R.tagmgn + R.tagmdl + R.tagdnd) AS angsttl,
+    TOFTABC.sahirrp,
+    TOFTABC.saldoblok,
+    TOFLMB.colbaru,
+    TOFLMB.kdcab
+FROM
+    TOFLMB
+LEFT JOIN
+    mCIF ON TOFLMB.nocif = mCIF.nocif
+LEFT JOIN
+    TOFTABC ON TOFLMB.acdrop = TOFTABC.notab
+LEFT JOIN
+    MCIFJOB ON mCIF.nocif = MCIFJOB.nocif
+LEFT JOIN
+    RankedTOFRS R ON TOFLMB.nokontrak = R.nokontrak AND R.row_num = 1
+WHERE
+    (TOFLMB.nama LIKE '%' + @nama + '%' OR TOFLMB.nokontrak LIKE @nama)
+    AND TOFLMB.stsrec = 'A'
+    AND TOFLMB.kdprd IN (11, 12, 13, 16, 21, 22, 23, 25)
+GROUP BY
+    TOFLMB.tgleff,
+    TOFLMB.nokontrak,
+    TOFLMB.acdrop,
+    TOFLMB.nama,
+    mCIF.alamat,
+    mCIF.kelurahan,
+    mCIF.kecamatan,
+    mCIF.kota,
+    mCIF.hp,
+    MCIFJOB.namapt,
+    MCIFJOB.alamat,
+    MCIFJOB.kota,
+    TOFLMB.kdprd,
+    TOFLMB.kdaoh,
+    TOFLMB.frekmdl,
+    TOFLMB.mdlawal,
+    TOFLMB.mgnawal,
+    R.tagmgn,
+    R.tagmdl,
+    R.tagdnd,
+    R.tgltagih,
+    R.os,
+    TOFTABC.sahirrp,
+    TOFTABC.saldoblok,
+    TOFLMB.colbaru,
+    TOFLMB.kdcab,
+    TOFLMB.tglakad
+ORDER BY
+    TOFLMB.tglakad DESC;
   `;
 
   const request = dbPool.request();
